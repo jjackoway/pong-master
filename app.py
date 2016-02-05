dab0c7928a2663ab0d7566fb922e9cc6a87d414b
Kevin Kuhl <kevin@wayblazer.com>
2016-02-04 21:21:36 -0600

    more improvements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, request
 from flask_restful import Resource, Api, reqparse
 from bson import json_util, CodecOptions, SON
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 from trueskill import Rating, rate_1vs1, TrueSkill
 from tabulate import tabulate
 import datetime, os, requests
@@ -11,8 +11,6 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('token')
 parser.add_argument('text')
-parser.add_argument('response_url')
-
 
 env = TrueSkill(draw_probability = 0.0, backend = 'mpmath', tau = .41666666667)
 env.make_as_global()
@@ -27,16 +25,13 @@ class Root(Resource):
         print request.data
         print request.headers
         print args['text']
-        print args['response_url']
         text = args['text'].lower().split()
         subcommand = text[0]
         print "Subcommand: " + subcommand
-        callback_url = args['response_url']
 
         #Register a new user
         if subcommand == 'register':
             name = text[1]
-            print "Name to register: " + name
             this_player = players.find_one({'name': name})
             if(this_player):
                 return this_player['name']+ ' already exists.'
@@ -51,18 +46,30 @@ class Root(Resource):
             for name in names:
                 result = players.delete_one({'name': name})
                 if result > 0:
-                    deleted.push(name)
-            if(deleted.length == 0):
+                    deleted.append(name)
+            if(len(deleted) == 0):
                 return "Couldn't find the player(s) you entered."
-            deleted.join(',')
+            deleted = ','.join(deleted)
             return "Kaboom. No more "+deleted+'.'
-
         elif subcommand == 'scores':
-            return Response('```\n'+tabulate(players.find(), headers='keys', tablefmt='fancy_grid')+'\n```\n', mimetype='text/plain')
+            return_scores = []
+            scores = players.find().sort('score', DESCENDING)
+            headers = ['Name','Score','Mu','Sigma']
+            for score in scores:
+                return_score = [
+                    score['name'],
+                    score['score'],
+                    score['mu'],
+                    score['sigma']
+                ]
+                return_scores.append(return_score)
+            return Response('```\n'+tabulate(return_scores, headers=headers, tablefmt='fancy_grid')+'\n```\n', mimetype='text/plain')
+        elif subcommand == 'games':
+            return Response('```\n'+tabulate(games.find(), headers='keys', tablefmt='fancy_grid')+'\n```\n', mimetype='text/plain')
         elif subcommand == 'record':
             player1 = text[1]
             operator = text[2]
-            player3 = text[3]
+            player2 = text[3]
 
             if operator != '>' and operator != '<':
                 return "Sorry, who won? Should be like 'player1' > 'player2'."
@@ -106,38 +113,7 @@ class Root(Resource):
             else:
                 return "Please pass in valid player names"
 
-
-class Games(Resource):
-    def post(self):
-        args = parser.parse_args()
-        winner_name = args['winner']
-        loser_name = args['loser']
-        if winner_name and loser_name:
-            winner = players.find_one({'name': winner_name})
-            loser = players.find_one({'name': loser_name})
-            if winner and loser:
-                winner_rating = Rating(winner['mu'], winner['sigma'])
-                loser_rating = Rating(loser['mu'], loser['sigma'])
-                winner_rating, loser_rating = rate_1vs1(winner_rating, loser_rating)
-
-                new_winner = {'name': winner['name'], 'mu': winner_rating.mu, 'sigma': winner_rating.sigma, 'score': floor(winner_rating.mu, winner_rating.sigma)}
-                players.replace_one({'name': winner['name']}, new_winner)
-                new_loser = {'name': loser['name'], 'mu': loser_rating.mu, 'sigma': loser_rating.sigma, 'score': floor(loser_rating.mu, loser_rating.sigma)}
-                players.replace_one({'name': loser['name']}, new_loser)
-
-                game = {'winner': winner['name'], 'loser': loser['name'], 'date': datetime.datetime.utcnow()}
-                games.insert_one(game)
-                return Response(json_util.dumps(game), mimetype='application/json')
-            else:
-                return "Players Not Found", 400
-        else:
-            return "Please pass in player names", 400
-    def get(self):
-        return Response(json_util.dumps(players.find()), mimetype='application/json')
-
-
 api.add_resource(Root, '/')
-# api.add_resource(Games, '/games')
 
 if __name__ == '__main__':
     opts = CodecOptions(document_class=SON)
